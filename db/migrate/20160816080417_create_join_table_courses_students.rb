08cbd9c4b351a0d9038e7f13e1bfdda61e3e9b03
nienkebos <nienkebos@live.com>
2016-08-16 10:04:53 +0200

    Created models and jointable

---
+++ db/migrate/20160816080417_create_join_table_courses_students.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableCoursesStudents < ActiveRecord::Migration[5.0]
+  def change
+    create_join_table :courses, :students do |t|
+      # t.index [:course_id, :student_id]
+      # t.index [:student_id, :course_id]
+    end
+  end
+end
